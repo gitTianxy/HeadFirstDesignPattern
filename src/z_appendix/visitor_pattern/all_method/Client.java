1cefb6675f14af9bd6f0228a627165a6df7034c4
gitTianxy <txy_learning@163.com>
2017-09-04 00:45:46 +0800

    update: visitor pattern interpretation

--- src/z_appendix/visitor_pattern/all_method/Client.java
+++ src/z_appendix/visitor_pattern/all_method/Client.java
@@ -2,8 +2,8 @@ package z_appendix.visitor_pattern.all_method;
 
 /**
  * 这种情况下: 
- * 1. visitor中需要定义不同类型Element作为参数的visit方法 
- * 2. element中需要定义以Visitor作为参数的accept函数, 且对element的访问形式转换element接受visitor
+ * 1. visitor中需要对不同element分别定义visit方法(其入参即为不同类型Element)
+ * 2. element中需要定义以Visitor作为参数的accept函数, 即访问形式由'visitor访问element'转变成'element接受visitor的访问'
  * 
  * @author kevin
  * @date 2017-08-06
